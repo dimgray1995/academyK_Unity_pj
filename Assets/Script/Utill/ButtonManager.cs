7d4db9207b0bc6ac248c1963fa16841fbcecc1ba
dimgray1995 <dimgray1995@gmail.com>
2025-02-05 22:25:52 +0900

    Day01

---
+++ Assets/Script/Utill/ButtonManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+
+    public void ReStartButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GotoMain()
+    {
+        SceneManager.LoadScene("MainDisplay");
+    }
+
+    public void GameExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
